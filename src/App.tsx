2f928eff511b3a2859e5f4ed0ee920ae6999df91
Angela <angdkay@gmail.com>
2025-09-08 14:21:28 -0600

    Create landing html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import './Theme.css';
 import { Header } from './components/Header';
+import { Landing } from './pages/Landing';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Landing/>
     </>
   )
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,23 @@
+import './Landing.css';
+
+export function Landing() {
+  return (
+    <>
+      <section id="landing">
+        <div className="landing-image">
+          
+        </div>
+        <div className="landing-info">
+          <h1>
+            CORDSA is a student-led affiliate of the Canadian Organization for Rare Disorders (CORD) at the University of Alberta.
+          </h1>
+          <div className="line"></div>
+          <p>
+            Our mission is to raise awareness for rare disorders, support research and education, and engage with community both on and off campus through advocacy and outreach. 
+          </p>
+          <button>learn more</button>
+        </div>
+      </section>
+    </>
+  )
+}
