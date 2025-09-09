d5055ced06ea1c67488960b8d8043e1553fa5594
Angela <angdkay@gmail.com>
2025-09-09 13:18:13 -0600

    Style newsletter section

--- src/pages/home-page/newsletter/Newsletter.tsx
+++ src/pages/home-page/newsletter/Newsletter.tsx
@@ -4,9 +4,11 @@ export function Newsletter() {
   return (
     <>
       <section id="news-letter">
-        <h1>Join our <b>Newsletter</b></h1>
-        <input type="text" placeholder="email"/>
-        <button>Subscribe</button>
+        <h1>Join our <b>newsletter</b></h1>
+        <div className="news-email">
+          <input type="text" placeholder="email"/>
+          <button>Subscribe</button>
+        </div>
       </section>
     </>
   )
