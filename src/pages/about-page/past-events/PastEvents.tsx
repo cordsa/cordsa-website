69ced1ef75ec44ac3c58d9efdfbd6f8d6e1c3419
Angela <angdkay@gmail.com>
2025-09-10 01:43:56 -0600

    Add past events data and images

---
+++ src/pages/about-page/past-events/PastEvents.tsx
@@ -0,0 +1,13 @@
+import './PastEvent.css';
+
+export function PastEvent() {
+  return (
+    <>
+      <section id="past-event">
+        <div className="events-grid">
+          
+        </div>
+      </section>
+    </>
+  )
+}
