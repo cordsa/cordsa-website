eb234fcefa32c3fccf017c26ffdefd9da2df3f23
Angela <angdkay@gmail.com>
2025-09-10 23:03:31 -0600

    Style donate form section

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import { About } from './pages/about-page/About';
 import { Contact } from './pages/contact-page/Contact';
 import { Resources } from './pages/resources-page/Resources';
 import { Involved } from './pages/involve-page/Involved';
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
     <>
       <Header/>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/about" element={<About/>}/>
--- src/pages/involve-page/donate-form/DonateForm.tsx
+++ src/pages/involve-page/donate-form/DonateForm.tsx
@@ -1,40 +1,51 @@
 import './DonateForm.css';
+import paypal from "../../../assets/img/icons/paypal.png";
 
 export function DonateForm() {
   return (
     <>
-      <section id="donate">
+<section id="donate-form">
         <div className="donate-box">
           <h1>Donate Online</h1>
+          <h2>Donation Info</h2>
           <form className="donate-form" action="#" method="post">
             <div className="form-row">
-              <label htmlFor="firstName">First Name *</label>
-              <input type="text" id="firstName" name="firstName" required />
+              <div className="first-name">
+                <label htmlFor="firstName">First Name <span>*</span></label>
+                <input type="text" id="firstName" name="firstName" required />
+              </div>
 
-              <label htmlFor="lastName">Last Name *</label>
-              <input type="text" id="lastName" name="lastName" required />
+              <div className="last-name">
+                <label htmlFor="lastName">Last Name <span>*</span></label>
+                <input type="text" id="lastName" name="lastName" required />
+              </div>
             </div>
 
             <div className="form-row">
-              <label htmlFor="donorType">Donor Type *</label>
-              <input type="text" id="donorType" name="donorType" required />
+              <div className="donor-type">
+                <label htmlFor="donorType">Donor Type <span>*</span></label>
+                <input type="text" id="donorType" name="donorType" required />
+              </div>
 
-              <label htmlFor="address">Address *</label>
-              <input type="text" id="address" name="address" required />
+              <div className="address">
+                <label htmlFor="address">Address <span>*</span></label>
+                <input type="text" id="address" name="address" required />
+              </div>
             </div>
 
             <div className="form-row">
-              <label htmlFor="company">Company Name / Group</label>
-              <input type="text" id="company" name="company" />
+              <div className="donor-group-name">
+                <label htmlFor="company">Company Name / Group</label>
+                <input type="text" id="company" name="company" />
+              </div>
             </div>
 
             <h3>Payment</h3>
             <div className="payment-options">
-              <button type="button" className="paypal-btn">
-                <img src="https://www.paypalobjects.com/webstatic/icon/pp258.png" alt="PayPal" />
-                PayPal
+              <button type="button" className="paypal-button">
+                <img src={paypal} alt="paypal logo" />
               </button>
-              <button type="button" className="card-btn">Donate with card</button>
+              <button type="button" className="card-button">Donate with card</button>
             </div>
           </form>
         </div>
